f65e00db43ef1ce83c20583245b095377e2971bd
matteo langlois <langlois.matteo49@gmail.com>
2024-05-22 18:08:52 +0200

    Improve flight printing

--- src/model/Flight.py
+++ src/model/Flight.py
@@ -1,9 +1,9 @@
 from sqlalchemy import *
 from sqlalchemy.orm import *
 from src.model import base, session, Aeroport, Reservation
-
 from typing import List
 from datetime import datetime
+from src.model.Aeroport import Aeroport
 
 
 class Flight(base):
@@ -14,13 +14,17 @@ class Flight(base):
     ArrivalDate = Column(DateTime)
     PlaneName = Column(String(50))
     FightCapacity = Column(Integer)
-    idFlightCompany = Column(Integer, ForeignKey('FlightCompany.idFlightCompany'))
+    idFlightCompany = Column(Integer,
+                             ForeignKey('FlightCompany.idFlightCompany'))
     idDepartureAeroport = Column(Integer, ForeignKey('Airport.idAeroport'))
     idArrivalAeroport = Column(Integer, ForeignKey('Airport.idAeroport'))
 
-    flight_flightcompany = relationship('FlightCompany', backref='flight_flightcompany')
-    departure_aeroport = relationship('Aeroport', backref='departure_aeroport', foreign_keys='Flight.idDepartureAeroport')
-    arrival_aeroport = relationship('Aeroport', backref='arrival_aeroport', foreign_keys='Flight.idArrivalAeroport')
+    flight_flightcompany = relationship('FlightCompany',
+                                        backref='flight_flightcompany')
+    departure_aeroport = relationship('Aeroport', backref='departure_aeroport',
+                                      foreign_keys='Flight.idDepartureAeroport')
+    arrival_aeroport = relationship('Aeroport', backref='arrival_aeroport',
+                                    foreign_keys='Flight.idArrivalAeroport')
 
     def __init__(self, Price, DepartureDate, ArrivalDate, PlaneName,
                  FlightCapacity, idFlightCompany, idDepartureAeroport,
@@ -48,12 +52,15 @@ class Flight(base):
                 f"{self.ArrivalDate}>")
 
     def __str__(self):
-        return (f"Vol {self.idFlight} de {self.idDepartureAeroport} à "
-                f"{self.idArrivalAeroport} le {self.DepartureDate}")
+        return (f"Vol de {Aeroport.get_by_id(self.idDepartureAeroport)} à"
+                f"{Aeroport.get_by_id(self.idArrivalAeroport)}, le "
+                f"{self.DepartureDate} jusqu'à {self.ArrivalDate}")
 
     def print(self):
-        return (f"{self.idDepartureAeroport} : {self.DepartureDate} - "
-                f"{self.idArrivalAeroport} ")
+        depart = Aeroport.get_by_id(self.idDepartureAeroport)
+        arrival = Aeroport.get_by_id(self.idArrivalAeroport)
+        return (f"{depart} : {self.DepartureDate} - "
+                f"{arrival} ")
 
     @staticmethod
     def get_by_id(idFlight: int) -> 'Flight':
@@ -65,19 +72,27 @@ class Flight(base):
 
     @staticmethod
     def get_departure_airport(idFlight: int) -> 'Aeroport':
-        return session.query(Aeroport).join(Flight).filter(Flight.idDepartureAeroport == Aeroport.idAeroport).filter(Flight.idFlight == idFlight).first()
+        return session.query(Aeroport).join(Flight).filter(
+            Flight.idDepartureAeroport == Aeroport.idAeroport).filter(
+            Flight.idFlight == idFlight).first()
 
     @staticmethod
     def get_arrival_airport(idFlight: int) -> 'Aeroport':
-        return session.query(Aeroport).join(Flight).filter(Flight.idArrivalAeroport == Aeroport.idAeroport).filter(Flight.idFlight == idFlight).first()
+        return session.query(Aeroport).join(Flight).filter(
+            Flight.idArrivalAeroport == Aeroport.idAeroport).filter(
+            Flight.idFlight == idFlight).first()
 
     @staticmethod
-    def get_flights_by_departure_airport(idDepartureAeroport: int) -> List['Flight']:
-        return session.query(Flight).filter(Flight.idDepartureAeroport == idDepartureAeroport).all()
+    def get_flights_by_departure_airport(idDepartureAeroport: int) -> List[
+        'Flight']:
+        return session.query(Flight).filter(
+            Flight.idDepartureAeroport == idDepartureAeroport).all()
 
     @staticmethod
-    def get_flights_by_arrival_airport(idArrivalAeroport: int) -> List['Flight']:
-        return session.query(Flight).filter(Flight.idArrivalAeroport == idArrivalAeroport).all()
+    def get_flights_by_arrival_airport(idArrivalAeroport: int) -> List[
+        'Flight']:
+        return session.query(Flight).filter(
+            Flight.idArrivalAeroport == idArrivalAeroport).all()
 
     @staticmethod
     def get_flights_by_date(date: datetime) -> List['Flight']:
@@ -85,23 +100,29 @@ class Flight(base):
 
     @staticmethod
     def get_flights_by_company(idFlightCompany: int) -> List['Flight']:
-        return session.query(Flight).filter(Flight.idFlightCompany == idFlightCompany).all()
+        return session.query(Flight).filter(
+            Flight.idFlightCompany == idFlightCompany).all()
 
     @staticmethod
     def get_future_flights() -> List['Flight']:
-        return session.query(Flight).filter(Flight.DepartureDate >= datetime.now()).all()
+        return session.query(Flight).filter(
+            Flight.DepartureDate >= datetime.now()).all()
 
     @staticmethod
     def get_flight_under_price(price: float) -> List['Flight']:
         return session.query(Flight).filter(Flight.Price <= price).all()
 
     @staticmethod
-    def get_flight_departure_arrival_airport(idDepartureAeroport: int, idArrivalAeroport: int) -> List['Flight']:
-        return session.query(Flight).filter(Flight.idDepartureAeroport == idDepartureAeroport).filter(Flight.idArrivalAeroport == idArrivalAeroport).all()
+    def get_flight_departure_arrival_airport(idDepartureAeroport: int,
+                                             idArrivalAeroport: int) -> List[
+        'Flight']:
+        return session.query(Flight).filter(
+            Flight.idDepartureAeroport == idDepartureAeroport).filter(
+            Flight.idArrivalAeroport == idArrivalAeroport).all()
 
     def get_reservations(self) -> List['Reservation']:
-        return session.query(Reservation).filter(Reservation.idFlight == self.idFlight).all()
+        return session.query(Reservation).filter(
+            Reservation.idFlight == self.idFlight).all()
 
     def get_free_seats(self) -> int:
         return self.FightCapacity - len(self.get_reservations())
-
