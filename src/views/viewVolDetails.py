41e544b30ce7d5107c54e8f229b204861dbf32c3
RomainGames <romainromanwattpad@gmail.com>
2024-05-27 15:42:22 +0200

    ajout listbox vol équivalent

--- src/views/viewVolDetails.py
+++ src/views/viewVolDetails.py
@@ -1,7 +1,9 @@
-from src.model import Flight
 from src.views.view import view
 import tkinter as vtk
 from src.views import viewInscription
+from datetime import timedelta
+from src.model import Flight
+import src.model as db
 
 """
 Classe viewVolDetails
@@ -18,27 +20,49 @@ class viewVolDetails(view):
         self.title = "Détails du vol"
         self.label_vol = None
         self.vol = None
+        self.dict_vol = None
 
     def init_widget(self):
         self.init_header()
-
+        self.dict_vol = {}
         label_title = vtk.Label(self, self.font_style("label"),
                                 text="Détails du vol")
         label_title.grid(column=0, row=1, pady=10, padx=10)
 
         self.label_vol = vtk.Label(self, self.font_style("label"), text="")
         self.label_vol.grid(column=0, row=2, pady=10, padx=10)
+        if self.controller.is_connected:
+            button_reserver = vtk.Button(self, self.button_style(), text="Réserver",
+                                         command=lambda: self.controller.reserver(self.vol))
+            button_reserver.grid(column=0, row=3, pady=10, padx=10)
 
-        button_reserver = vtk.Button(self, self.button_style(), text="Réserver",
-                                     command=lambda: self.controller.reserver(self.vol))
-        button_reserver.grid(column=0, row=3, pady=10, padx=10)
+        label_otherflights = vtk.Label(self, self.font_style("label"),
+                                        text="Autres vols disponibles")
+        label_otherflights.grid(column=0, row=4, pady=10, padx=10)
 
-        button_retour = vtk.Button(self, self.button_style(), text="Retour",
-                                   command=lambda:
-                                   self.controller.show_frame(
-                                       viewInscription.viewInscription))
-        button_retour.grid(column=0, row=4, pady=10, padx=10)
+        self.listbox_otherflights = vtk.Listbox(self, selectmode=vtk.SINGLE,
+            width = 100, height = 10, font = self.font_style("subtitle"))
+        self.listbox_otherflights.grid(column=0, row=5, pady=10, padx=10)
+        self.listbox_otherflights.bind('<ButtonRelease-1>', lambda
+            event: self.details(self.listbox_otherflights, event))
 
-    def change_data(self, vol: 'Flight'):
-        self.label_vol.config(text=vol)
+
+    def change_data(self, vol: Flight):
         self.vol = vol
+        self.label_vol.config(text=vol)
+
+        vols = db.Flight.get_all()
+        for index, flight in enumerate(vols):
+            if flight != self.vol:
+                if (flight.departure_aeroport == self.vol.departure_aeroport) and (
+                        flight.arrival_aeroport == self.vol.arrival_aeroport):
+                    if (self.vol.DepartureDate - timedelta(days=3)) <= vol.DepartureDate <= (
+                            self.vol.DepartureDate + timedelta(days=3)):
+                        self.dict_vol[index] = flight
+                        self.listbox_otherflights.insert(vtk.END, flight)
+
+    def details(self, listbox, event):
+        selected_index = listbox.nearest(event.y)
+        if selected_index != -1:
+            listbox.delete(0, vtk.END)
+            self.change_data(self.dict_vol[selected_index+1])
